6650e76a69a0e1f56309360141c79eaf02412897
jessicayung <jy357@cam.ac.uk>
2016-01-11 22:35:24 +0000

    Init

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class PagesController extends Controller
+{
+    //
+    public function contact() {
+		return view('pages.contact');
+	}
+
+	public function index() {
+		return view('pages.index');
+	}
+}
---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,16 @@
+@extends('app')
+
+@section('content')
+
+<h1>Contact Me</h1>
+
+<p>
+    <ul>
+        <li>Send me an email</li>
+        <li>Fill in the feedback form below</li> 
+        <li>Contribute</li>
+    </ul>
+</p>
+
+
+@stop
