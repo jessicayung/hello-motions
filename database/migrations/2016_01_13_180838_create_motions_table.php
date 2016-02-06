8b19eebf9a1ebc7122cbe8d6c1f922c22b7c2686
jessicayung <jy357@cam.ac.uk>
2016-02-06 20:20:03 +0000

    Motions_Table Database

--- database/migrations/2016_01_13_180838_create_motions_table.php
+++ database/migrations/2016_01_13_180838_create_motions_table.php
@@ -12,7 +12,7 @@ class CreateMotionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('motions', function (Blueprint $table) {
+        Schema::create('Motions_Table', function (Blueprint $table) {
             $table->increments('id');
             $table->date('Date');
             $table->text('Circuit', 50);
