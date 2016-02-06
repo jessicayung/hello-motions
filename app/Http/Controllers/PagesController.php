a4012d689a4c8c0486537b3c7cdf75f836555ca7
jessicayung <jy357@cam.ac.uk>
2016-02-06 22:58:07 +0000

    In query: Column name is Round_Code, not RoundCode

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -26,9 +26,9 @@ class PagesController extends Controller
 
 	public function index() {
 		$recentmotions = DB::table('motions', [25])
-							-> select('Date', 'RoundCode', 'Round', 'Motion', 'Tournament') 
+							-> select('Date', 'Round_Code', 'Round', 'Motion', 'Tournament') 
 							-> orderby('Date', 'desc') 
-							-> orderby('RoundCode', 'asc') 
+							-> orderby('Round_Code', 'asc') 
 							-> get();
         return view('pages.index', ['motions' => $recentmotions]);
 	}
@@ -39,11 +39,12 @@ class PagesController extends Controller
 
 	public function motions2015() {
 		$motions2015 = DB::table('motions')
+				-> select('Date', 'Round_Code', 'Round', 'Motion', 'Tournament') 
                 ->groupBy('Date')
                 ->having('Date', '>=', 20150101)
                 ->having('Date', '<', 20160101)
                 -> orderby('Date', 'desc') 
-				-> orderby('RoundCode', 'asc') 
+				-> orderby('Round_Code', 'asc') 
                 ->get();
         return view('pages.motions2015', ['motions' => $motions2015]);
 
