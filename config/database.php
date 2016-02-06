2ea883bd5bcac733416fe50abaa5843b8abe58b8
jessicayung <jy357@cam.ac.uk>
2016-02-06 20:02:11 +0000

    Hard code db thing to test

--- config/database.php
+++ config/database.php
@@ -51,7 +51,7 @@ return [
             'host'      => env('DB_HOST', 'localhost'),
             'database'  => env('DB_DATABASE', 'forge'),
             'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
+            'password'  => env('DB_PASSWORD', '7vJCHz5SD2Qq3fD9rkKb'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
