50c579938770f68c38504985f73fe7fcd302d73a
jessicayung <jy357@cam.ac.uk>
2016-02-06 23:43:13 +0000

    Motion model edit (fillable)

--- app/Motion.php
+++ app/Motion.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Motion extends Model
 {
-    //
+    protected $fillable = [
+
+    	'motion',
+    	'tournament',
+    	'Date',
+    	'Circuit',
+    	'Tournament',
+    	'CA_1',
+    	'CA_2',
+    	'CA_3',
+    	'CA_4',
+    	'CA_5',
+    	'CA_6',
+    	'CA_7',
+    	'Round_Code',
+    	'Round',
+    	'Motion',
+    	'Infoslide',
+    	'Topic_Area_1',
+    	'Topic_Area_2'
+
+    ]
 }
