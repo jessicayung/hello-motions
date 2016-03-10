75266bfef9ed08f018452e2cad9bef80c0b67e15
jessicayung <jy357@cam.ac.uk>
2016-03-10 09:22:09 +0000

    Update search forms

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -13,16 +13,14 @@
 	<h1>Search for Motions</h1>
 	<p>Search for motions by keyword, category, Chief Adjudicator, tournament, or timeframe. <a href="https://docs.google.com/spreadsheets/d/1qQlqFeJ3iYbzXYrLBMgbmT6LcJLj6JcG3LJyZSbkAJY/edit?usp=sharing">Access the motions spreadsheet directly here</a>. </p>
 
-	<form action="search.php?go" method="get" id="searchform" >
-	    <div class="form-group, col-md-6">
-	        <input name="q" class="form-control" type="text" placeholder="e.g. International Relations, WUDC, Cambridge IV">
-	    </div>
-	    <p>
-	    	<div class="form-group">
-	    		<button type="submit" name = "submit" value=1 class="btn btn-success">Search</button>
-	   		</div>
-		</p>
-	</form>
+     <form action="search" method="get" id="search-motions" >
+        <div class="form-group, col-md-6">
+            <input class="form-control" type="text" name="q" placeholder="e.g. International Relations, WUDC, Cambridge IV">
+        </div>
+        <p><div class="form-group">
+        <button type="submit" name="search-motions" class="btn btn-primary">Search</button>
+        </div></p>
+      </form>
 
 	@if (count($motions) === 0)
 
