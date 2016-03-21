0424b1d3e35988117b8a619f31d8408d8ebe8939
jessicayung <jy357@cam.ac.uk>
2016-03-21 17:22:26 +0000

    For some reason the views didn't sync when I was committing in Terminal?

---
+++ resources/views/pages/motions2013andprevious-mobile.blade.php
@@ -0,0 +1,29 @@
+@extends('template')
+
+@section('title')
+
+  2015 Motions
+
+@endsection
+
+@section('content')  
+
+  <h1>2015 Motions</h1>
+    <p>Listed with most recent tournaments first. Can't find a tournament? <a href="https://docs.google.com/forms/d/14v5RuG6NHjDySrLN8zJC97-e761sRBivhOoPxjt0K6I/viewform?c=0&w=1">Send us the motions here</a>. </p>
+    <p> <a href="motions2013andprevious">Full version</a></p>
+
+  <table class="table table-striped">
+    <tr>
+     <th>Tournament</th>
+     <th>Motion</th>
+    </tr>
+    <!--Output data of each row-->
+    @foreach ($motions2013prev as $motion)
+      <tr>
+        <td> {{ $motion ->Tournament }} </td>
+        <td> {{ $motion ->Motion }} </td>
+      </tr>
+    @endforeach
+   </table>
+
+@endsection
