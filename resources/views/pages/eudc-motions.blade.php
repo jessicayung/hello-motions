717be9b5d2e7e275000e8c36ca4f8b80a110ea3d
Jessica Yung <jessicayungyc@gmail.com>
2019-09-17 17:37:11 +0800

    format(eudc-motions): bold CA team subtitle

--- resources/views/pages/eudc-motions.blade.php
+++ resources/views/pages/eudc-motions.blade.php
@@ -19,7 +19,7 @@
   </p>
 
   <h3 id="eudc2019">2019 Athens EUDC</h3>  
-  <p>CA Team: Sharmila Parmanand, Dan Lahav, Benji Kalman, Brian Wong, Clíodhna Ní Ch, Milos Marjanovic</p>
+  <p><b>CA Team</b>: Sharmila Parmanand, Dan Lahav, Benji Kalman, Brian Wong, Clíodhna Ní Ch, Milos Marjanovic</p>
  
   <table class="table table-striped">
     <tr>
@@ -37,7 +37,7 @@
    </table>
 
   <h3 id="eudc2018">2018 Novi Sad EUDC</h3>  
-  <p>CA Team: Duncan Crowe, Olivia Sundberg Diez, Daan Welling, Dee Courtney, Gigi Gil, Ilija Ivanisevic, Yair Har-Oz</p>
+  <p><b>CA Team</b>: Duncan Crowe, Olivia Sundberg Diez, Daan Welling, Dee Courtney, Gigi Gil, Ilija Ivanisevic, Yair Har-Oz</p>
  
   <table class="table table-striped">
     <tr>
@@ -56,7 +56,7 @@
 
 
   <h3 id="eudc2017">2017 Tallinn EUDC</h3>  
-  <p>CA Team: Steven Ramesh Rajavinothan, Helena Ivanov, Dan Lahav, Rebecca Irvine, Thomas Simpson, Olivia Sundberg</p>
+  <p><b>CA Team</b>: Steven Ramesh Rajavinothan, Helena Ivanov, Dan Lahav, Rebecca Irvine, Thomas Simpson, Olivia Sundberg</p>
  
   <table class="table table-striped">
     <tr>
@@ -74,7 +74,7 @@
    </table>
 
   <h3 id="eudc2016">2016 Warsaw EUDC</h3>  
-  <p>CA Team: Emilia Carlqvist, Harish Natarajan, Adam Hawksbee, Helena Ivanov, Radu Cotarcea, Yael Bezalel</p>
+  <p><b>CA Team</b>: Emilia Carlqvist, Harish Natarajan, Adam Hawksbee, Helena Ivanov, Radu Cotarcea, Yael Bezalel</p>
  
   <table class="table table-striped">
     <tr>
@@ -92,7 +92,7 @@
    </table>
 
   <h3 id="eudc2015">2015 Vienna EUDC</h3>
-  <p>CA Team: Christine Simpson, Michael Shapira, Amanda Moorghen, Emilia Carlqvist, Gavin Illsley</p>
+  <p><b>CA Team</b>: Christine Simpson, Michael Shapira, Amanda Moorghen, Emilia Carlqvist, Gavin Illsley</p>
 
   <table class="table table-striped">
     <tr>
@@ -110,7 +110,7 @@
    </table>
 
   <h3 id="eudc2014">2014 Zagreb EUDC</h3>
-  <p>CA team: Rebecca Meredith, Milan Vignjevic, Alon van Dam, Ashish Kumar,  Engin Arikan, Maja Cimerman, Sella Nevo, Tomas Beerthuis</p>
+  <p><b>CA team</b>: Rebecca Meredith, Milan Vignjevic, Alon van Dam, Ashish Kumar,  Engin Arikan, Maja Cimerman, Sella Nevo, Tomas Beerthuis</p>
 
 	<table class="table table-striped">
     <tr>
@@ -128,7 +128,7 @@
    </table>
 
   <h3 id="eudc2013">2013 Manchester EUDC</h3>
-  <p>CA Team: Shengwu Li, Omer Nevo, Joe Roussos, Dessislava Kirova, Filip Dobranic, Andrew Tuffin</p>
+  <p><b>CA Team</b>: Shengwu Li, Omer Nevo, Joe Roussos, Dessislava Kirova, Filip Dobranic, Andrew Tuffin</p>
 
   <table class="table table-striped">
     <tr>
@@ -146,7 +146,7 @@
    </table>
 
   <h3 id="eudc2012">2012 Belgrade EUDC</h3>
-  <p>CA Team: Art Ward, Isabelle Loewe, filip Dobranic, Stephen Boyle, Benjamin Woolgar, Manos Moschopoulos</p>
+  <p><b>CA Team</b>: Art Ward, Isabelle Loewe, filip Dobranic, Stephen Boyle, Benjamin Woolgar, Manos Moschopoulos</p>
   <!-- from http://eudc.wikidot.com/adjudication -->
 
   <table class="table table-striped">
